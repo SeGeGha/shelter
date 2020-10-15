2516b959202ecfcf68603ad2a14e2daee1367821
Sergey <s3gaddtv@gmail.com>
2020-10-15 14:55:24 +0300

    chore: update pets cards config

--- src/config/petsCards.ts
+++ src/config/petsCards.ts
@@ -1,6 +1,11 @@
-import KatrineImg from '../assets/img/pets-cat-katrine.png';
-import JenniferImg from '../assets/img/pets-dog-jennifer.png';
-import WoodyImg from '../assets/img/pets-dog-woody.png';
+import KatrineImg from '../assets/img/pets-cat-katrine.jpg';
+import JenniferImg from '../assets/img/pets-dog-jennifer.jpg';
+import WoodyImg from '../assets/img/pets-dog-woody.jpg';
+import SophiaImg from '../assets/img/pets-dog-sophia.jpg';
+import CharlyImg from '../assets/img/pets-dog-charly.jpg';
+import TimmyImg from '../assets/img/pets-cat-timmy.jpg';
+import ScarlettImg from '../assets/img/pets-dog-scarlett.jpg';
+import FreddieImg from '../assets/img/pets-cat-freddie.jpg';
 
 export default [
   {
@@ -20,5 +25,35 @@ export default [
     species: 'Dog',
     name: 'Woody',
     imgSrc: WoodyImg,
+  },
+  {
+    id: 4,
+    species: 'Dog',
+    name: 'Sophia',
+    imgSrc: SophiaImg,
+  },
+  {
+    id: 5,
+    species: 'Cat',
+    name: 'Timmy',
+    imgSrc: TimmyImg,
+  },
+  {
+    id: 6,
+    species: 'Dog',
+    name: 'Charly',
+    imgSrc: CharlyImg,
+  },
+  {
+    id: 7,
+    species: 'Dog',
+    name: 'Scarlett',
+    imgSrc: ScarlettImg,
+  },
+  {
+    id: 8,
+    species: 'Cat',
+    name: 'Freddie',
+    imgSrc: FreddieImg,
   },
 ];
