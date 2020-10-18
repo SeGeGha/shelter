82fabc2fbf7008787757cef909a0a9d29510bf88
Sergey Sudakov <s3gaddtv@gmail.com>
2020-10-18 20:33:27 +0300

    feat: create routing for logo

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Routes from '../../../config/routes';
 
 import './Logo.scss';
 
 const Logo: React.FC = () => (
-  <div className="logo">
+  <Link to={Routes.MainPage} className="logo">
     <h1 className="logo__title">Cozy House</h1>
     <p className="logo__subtitle">Shelter for pets in Boston</p>
-  </div>
+  </Link>
 );
 
 export default Logo;
